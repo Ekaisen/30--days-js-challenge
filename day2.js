e6ed91c493f0bbd4d35dac4521cc55c6871645e6
Ekaisen <aryanenigma4@gmail.com>
2024-07-14 19:10:00 +0530

    day 2
    day 2

---
+++ day2.js
@@ -0,0 +1,85 @@
+// Activity 1
+// Task 1
+
+const num1 = 12;
+const num2 = 13;
+console.log(`this is the sum of num1 and num2 ${num1 + num2}`)
+
+
+// Task 2
+
+const num3 = 12;
+const num4 = 13;
+console.log(`this is the difference of num3 and num4 ${num3 - num4}`)
+
+
+// Task 3
+
+const num5 = 12;
+const num6 = 13;
+console.log(`this is the product of num5 and num6 ${num5 * num6}`)
+
+// Task 4
+
+const num7 = 12;
+const num8 = 13;
+console.log(`this is the quotient of num7 and num8 ${num7 / num8}`)
+
+
+// Task 5
+
+const num9 = 12;
+const num1a = 13;
+console.log(`this is the remainder of num9 and num1a ${num9 % num1a}`) 
+
+
+// Activity 2 
+// Task 6
+
+var numb = 14;
+numb += 23;
+console.log(numb)
+
+// Task 7
+var numb1 = 24;
+numb1 -= 23;
+console.log(numb1)
+
+
+// Activity 3
+// Task 8 
+const rank1 = 111;
+const rank2 = 222;
+console.log(rank1>rank2) // false
+console.log(rank2>rank1) // true
+
+// Task 9 
+const a = 12;
+const b = 13;
+console.log(a<=b)
+console.log(a>=b)
+
+// task 10 
+const c1 = 99;
+var c2 = 99;
+console.log(c1==c2)
+console.log(c1===c2)
+
+// Activity 4
+// Task 11
+var d1 = 33;
+var d2 = 44;
+console.log(d1>40 && d2>40)
+// Task 12
+console.log(d1>40 || d2>40)
+// Task 13
+console.log(!(d1==d2))
+
+// Activity 5
+// Task 14
+
+var result = (12<1)?true:false;
+console.log(result)
+
+var happy = (100>12)?"yes":"no";
+console.log(happy)
